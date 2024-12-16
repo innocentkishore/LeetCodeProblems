e9bc1224393dba6d15896081d92ead2d94cefd5a
Kishore Kumar <160230351+innocentkishore@users.noreply.github.com>
2024-12-16 10:30:18 +0530

    [77] [Time Beats: 80.73%] [Memory Beats: 85.61%] - LeetPush

--- 77-Combinations.java
+++ 77-Combinations.java
@@ -1,21 +1,21 @@
 class Solution {
     public List<List<Integer>> li=new ArrayList<>();
     public List<Integer> list=new ArrayList<>();
-    public void backtrack(int level,int i,int n,int k){
+    public void backtrack(int i,int n,int k){
         if(list.size()==k){
             li.add(new ArrayList<>(list));
             return ;
         }
         for(int j=i;i<=n;i++){
             list.add(i);
-            backtrack(level+1,i+1,n,k);
+            backtrack(i+1,n,k);
             list.remove(list.size()-1);
         }
 
 
     }
     public List<List<Integer>> combine(int n, int k) {
-        backtrack(0,1,n,k);
+        backtrack(1,n,k);
         return li;
         
     }
