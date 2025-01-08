f69e854e6705a280182ecf4269fca6aaa693f7e8
Kishore Kumar <160230351+innocentkishore@users.noreply.github.com>
2025-01-08 19:18:00 +0530

    [3042] [Time Beats: 23.95%] [Memory Beats: 9.96%] - LeetPush

--- 3042-Count-Prefix-and-Suffix-Pairs-I.java
+++ 3042-Count-Prefix-and-Suffix-Pairs-I.java
@@ -6,8 +6,8 @@ class Solution {
                 if(words[i].length()<=words[j].length()){
                     String str1=words[j].substring(0,words[i].length());
                     String str2=words[j].substring(words[j].length()-words[i].length(),words[j].length());    
-                    System.out.println("Str1"+str1);
-                     System.out.println("Str2"+str2);
+                    // System.out.println("Str1"+str1);
+                    //  System.out.println("Str2"+str2);
 
                         if(str1.equals(words[i])&&str2.equals(words[i])){
                             c++;
