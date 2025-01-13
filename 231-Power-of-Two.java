d13384e6b7e3c925b9bfea89606b4899ea4cd81f
Kishore Kumar <160230351+innocentkishore@users.noreply.github.com>
2025-01-13 17:03:01 +0530

    [231] [Time Beats: 41.70%] [Memory Beats: 58.68%] - LeetPush

--- 231-Power-of-Two.java
+++ 231-Power-of-Two.java
@@ -2,6 +2,7 @@ class Solution {
     public boolean isPowerOfTwo(int n) {
           if (n <= 0) return false;
         if (n == 1) return true; // Base case: 3^0 = 1
-        return n % 2 == 0 && isPowerOfTwo(n / 2); // Recursively divide by 3
+        return (n > 0 && (n & (n - 1)) == 0);
+
     }
 }
