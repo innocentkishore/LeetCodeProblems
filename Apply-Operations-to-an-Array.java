501fe1241045ce94f1d11595a0656eb6187f181d
Kishore Kumar <160230351+innocentkishore@users.noreply.github.com>
2025-03-01 18:57:21 +0530

    [2460] [Time Beats: 96.37%] [Memory Beats: 5.12%] - LeetPush

---
+++ Apply-Operations-to-an-Array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] applyOperations(int[] nums) {
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]==nums[i+1]){
+                nums[i]=nums[i]*2;
+                nums[i+1]=0;
+            }
+        }
+        int []res=new int[nums.length];
+        int k=0;
+        for(int i:nums){
+            if(i!=0) res[k++]=i;
+        }
+        return res;
+    }
+}
