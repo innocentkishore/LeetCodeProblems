bd5c3dfd6070aa77517637090de9ce9ed667d551
Kishore Kumar <160230351+innocentkishore@users.noreply.github.com>
2025-03-03 21:12:38 +0530

    [2161] [Time Beats: 5.36%] [Memory Beats: 80.81%] - LeetPush

---
+++ Partition-Array-According-to-Given-Pivot.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] pivotArray(int[] nums, int pivot) {
+        int []res=new int[nums.length];
+        int k=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]<pivot){
+                res[k++]=nums[i];
+                System.out.print(k);
+            }
+
+        }
+         for(int i=0;i<nums.length;i++){
+            if(nums[i]==pivot){
+                res[k++]=nums[i];
+            }
+
+        }
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>pivot){
+                res[k++]=nums[i];
+            }
+
+        }
+        return res;
+    }
+}
